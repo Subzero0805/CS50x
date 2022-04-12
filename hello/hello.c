f37f1a4a74bed38938c4ce8fad8980dc13533ffe
Subzero0805 <68710182+Subzero0805@users.noreply.github.com>
2022-04-12 20:08:50 +0100

    Delete hello directory

--- hello/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    //intitalise variable (name) that stores the user's name.
-    string name = get_string("What is your name? ");
-    printf("hello, %s\n", name);
-}
